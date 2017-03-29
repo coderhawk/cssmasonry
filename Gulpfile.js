aacbf5aafc7f520e7522e94eeb7928dd21765f76
coderhawk <sandeep.msk@gmail.com>
2017-03-29 18:04:42 +0530

    implemented blocks and added images :)

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,45 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+
+
+// variables for css build
+var cssInput = './src/theme/default/scss/**/*.scss';
+var cssOutput = './dist/theme/default/css/';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions']
+};
+
 
 // make some css
 gulp.task('styles', function(){
-    gulp.src('./src/theme/default/scss/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('./dist/theme/default/css/'))
+    gulp.src(cssInput)
+    .pipe(sass(sassOptions).on('error',sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(cssOutput))
     .pipe(gulp.dest('./src/theme/default/css/'))
-     .pipe(browserSync.stream());
+    .pipe(browserSync.stream());
 });
 
+// variables for html build
+var htmlInput = './src/**/*.html';
+var htmlOutput = './dist/';
+
+// html changes
+gulp.task('markup', function(){
+  gulp.src(htmlInput)
+  .pipe(gulp.dest(htmlOutput))
+  .pipe(browserSync.stream());
+});
 
 // watch 
 gulp.task('default',function() {
@@ -23,4 +52,14 @@ gulp.task('default',function() {
       })
       
     gulp.watch('./src/theme/default/scss/**/*.scss',['styles']);
+     gulp.watch('./src/**/*.html',['markup']);
+});
+
+// only for release
+gulp.task('release', function () {
+  return gulp
+    .src(input)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
 });
